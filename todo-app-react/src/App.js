f3fedc663e75d2f8ce933432b15db5d5d080ccc2
Prashant Shahi <prashant@dgraph.io>
2020-05-15 03:30:05 +0530

    Adding Apollo client

--- todo-app-react/src/App.js
+++ todo-app-react/src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React from 'react'
+
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { createHttpLink } from "apollo-link-http";
+
 import './App.css';
 
+const createApolloClient = () => {
+  const httpLink = createHttpLink({
+    uri: "http://localhost:8080/graphql",
+    options: {
+      reconnect: true,
+    },
+  });
+
+  return new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+  });
+}
+
 const App = () => {
+  const client = createApolloClient();
   return (
-    <div>
-      <h1>todos</h1>
-      <input
-        className="new-todo"
-        placeholder="What needs to be done?"
-        autoFocus={true}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>todos</h1>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus={true}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
-export default App;
+export default App
--- todo-app-react/src/index.js
+++ todo-app-react/src/index.js
@@ -8,4 +8,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
