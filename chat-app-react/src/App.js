82e500cb06aa74338a0e5b397e0162df5c5c7e75
Akash Jain <akashjain971@gmail.com>
2020-07-04 17:28:01 +0530

    react chat app library changes

--- chat-app-react/src/App.js
+++ chat-app-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider } from 'react-apollo';
 import Post from './Post';
 import apolloClient from './ApolloSetup';
 
