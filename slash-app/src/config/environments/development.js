276eb4dba6f46581ef57d78147a9a99596210f88
Anupam Saha <anupam_saha@thoughtnext.com>
2020-07-16 16:14:31 +0530

    removed api-key and slash url

--- slash-app/src/config/environments/development.js
+++ slash-app/src/config/environments/development.js
@@ -1,4 +1,4 @@
 module.exports = {
     dgraph_server_url: "http://localhost:8080/graphql",
-    token: "Bearer AAAAAAAAAAAAAAAAAAAAALVGFwEAAAAALzcChr4Np92wlDqbuRA8fQneCuQ%3DqGFJ68aN1xKbOLuPrtaWbKhnMF26GsOEknN9iAxpq4xtik5eIi"
+    token: process.env.TWITTER_TOKEN
 }
--- slash-app/src/config/environments/production.js
+++ slash-app/src/config/environments/production.js
@@ -1,6 +1,4 @@
 module.exports = {
-    dgraph_server_url: "https://dangerous-acoustics-6933.us-west-2.aws.cloud.dgraph.io/graphql",
-    token: "Bearer AAAAAAAAAAAAAAAAAAAAALVGFwEAAAAALzcChr4Np92wlDqbuRA8fQneCuQ%3DqGFJ68aN1xKbOLuPrtaWbKhnMF26GsOEknN9iAxpq4xtik5eIi"
+    dgraph_server_url: process.env.REACT_APP_DGRAPH_SERVER_URL,
+    token: process.env.REACT_APP_TWITTER_TOKEN
 }
-
-//LU8xr9Fw8CW3AD8v/nsZtBbSp0dlTLr9v61fmBxHP/E=
