e3de371695f0bf33067894aa9451e51188c85f8a
Tejas Dinkar <tejas@gja.in>
2020-07-17 22:23:32 +0530

    Passing in stuff through variables

--- todo-app-react/src/index.js
+++ todo-app-react/src/index.js
@@ -17,8 +17,8 @@ const onRedirectCallback = appState => {
 
 ReactDOM.render(
   <Auth0Provider
-    domain={config.domain}
-    client_id={config.clientId}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN || config.domain}
+    client_id={process.env.REACT_APP_AUTH0_CLIENT_ID || config.clientId}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
   />,
