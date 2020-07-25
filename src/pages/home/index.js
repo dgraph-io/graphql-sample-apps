e0f26a45482d800ea0b8542eee1f61e1b5a3ce90
Tejas Dinkar <tejas@gja.in>
2020-07-26 00:17:52 +0530

    Putting the top link into a para

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -13,18 +13,8 @@ const Home = () => {
       <NavbarItem type='icon' iconName='settings.svg' oncClick={() => { }} />
     </Navbar>
     <Content>
-      <Link to="/types">See Types Here</Link>
       <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-        facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-        gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-        donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-        Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-        imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-        arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-        donec massa sapien faucibus et molestie ac.
+        <Link to="/types">See Types Here</Link>
       </Typography>
       <Typography paragraph>
         Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
