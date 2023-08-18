e59c63d1ce3bd75045f161167b169e39dcec2df5
Marcelo Dalta <marcelo.dalta@gmail.com>
2023-08-18 16:40:02 -0300

    Added new line and removed unnecessary comments

--- todo-app-react/src/components/modal.jsx
+++ todo-app-react/src/components/modal.jsx
@@ -6,7 +6,6 @@ export function MyModal({isOpen, onRequestClose, children}) {
   if (!isOpen) {
     return null;
   }
-  // console.log("Modal todo: " + todo)
   return (
     <Modal
       isOpen={isOpen}
@@ -18,7 +17,6 @@ export function MyModal({isOpen, onRequestClose, children}) {
           backgroundColor: 'rgba(0, 0 ,0, 0.25)'
         },
         content: {
-          // border: '1px solid #EF255A',
           background: '#ffffff',
           borderRadius: '20px',
           padding: '20px',
@@ -35,7 +33,6 @@ export function MyModal({isOpen, onRequestClose, children}) {
         {children}
       </Flex>
     </Modal>
-  // {children}
   )
 }
 
